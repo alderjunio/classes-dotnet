a15a3f97aa9059d1ccfd7a3779eb5d8024910aec
alderjunio <alder.junio@gmail.com>
2024-07-30 22:33:33 -0400

    Criado o método Apresentar na classe Pessoa

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -4,5 +4,10 @@ namespace dotnet.Models
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+        }
     }
 }
